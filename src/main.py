7553cb20d59c9c0148e1b976723c7ee095c57d04
Valentin_Luzgin <valentineluzgin@gmail.com>
2024-05-24 18:43:46 +0300

    реализован модуль processing и добавлен readme файл

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
-from src.widget import masked_cards_and_accounts, splited_str
-from src.widget import date_conversion
+from src.widget import masked_cards_and_accounts, date_conversion
 
 
-print(masked_cards_and_accounts(splited_str))
+bank_list = [
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+]
+
+
+for i in bank_list:
+    print(masked_cards_and_accounts(i))
+
+
 print(date_conversion("2018-07-11T02:26:18.671407"))
